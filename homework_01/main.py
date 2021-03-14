0ea25c62bcc32972e02b9c79266e048f5bcab2cf
Илющенко Михаил Сергеевич <mihail.ilyushchenko@goods.ru>
2021-03-14 21:28:15 +0300

    fix task 1

--- homework_01/main.py
+++ homework_01/main.py
@@ -3,10 +3,21 @@
 Функции и структуры данных
 """
 
+# Задание № 1
 
-def power_numbers(list_of_numbers):
+def my_map(func, list_of_values):
+    return (func(value) for value in list_of_values)
+
+
+def process_numbers(list_of_numbers):
     return [num ** 2 for num in list_of_numbers]
-    
+
+
+def power_numbers(*args):
+    return list(my_map(process_numbers, args))
+
+# Задание № 2
+
 # filter types
 ODD = "odd"
 EVEN = "even"
